ed5cfcc2dc307cb75fbbc9fa7f6da4b332b74798
Danil <112797010+JordennJd@users.noreply.github.com>
2025-08-23 18:58:32 +0300

    chore: change version

--- SaxsSpot.Shared.Tests/ProgressTrackerTests/ProgressTrackerTests.cs
+++ SaxsSpot.Shared.Tests/ProgressTrackerTests/ProgressTrackerTests.cs
@@ -18,13 +18,23 @@ namespace SaxsSpot.Shared.UnitTests
         }
 
         [Fact]
-        public async Task ProgressTrackerCrateTest()
+        public async Task ProgressTrackerCreateTest()
         {
             var client = _fixture.ServiceProvider.GetRequiredService<IJobServiceClient>();
             var jobId = "0127470f-f135-780b-9534-c3d5b59f319a";
 
             var job = await client.GetJobAsync(
                 new ProgressTrackerClient.Contracts.Models.GetJobQuery(jobId));
+        }
+        
+        [Fact]
+        public async Task ProgressTrackerChangeMessageTest()
+        {
+            var client = _fixture.ServiceProvider.GetRequiredService<IJobServiceClient>();
+            var jobId = "0127470f-f135-780b-9534-c3d5b59f319a";
+
+            var job = await client.ChangeJobMessageAsync(
+                new ProgressTrackerClient.Contracts.Models.ChangeJobMessageQuery(jobId, "test"));
         }
     }
 }

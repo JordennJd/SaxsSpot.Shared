9ccfe496fe4ec003f8610ce7f14c52dfd5dbb599
Danil <112797010+JordennJd@users.noreply.github.com>
2025-08-23 18:48:25 +0300

    fix

--- SaxsSpot.Shared.ProgressTrackerClient/Mapping/JobServiceProfiles.cs
+++ SaxsSpot.Shared.ProgressTrackerClient/Mapping/JobServiceProfiles.cs
@@ -9,7 +9,6 @@ namespace SaxsSpot.Shared.ProgressTrackerClient.Mapping
     {
         public JobServiceProfiles()
         {
-            // Общие настройки для всех маппингов
             CreateMap<Timestamp, DateTime>().ConvertUsing(t => t.ToDateTime());
             CreateMap<DateTime, Timestamp>().ConvertUsing(d => Timestamp.FromDateTime(d));
             CreateMap<Timestamp, DateTime?>().ConvertUsing(t => t == null ? null : t.ToDateTime());
@@ -28,12 +27,10 @@ namespace SaxsSpot.Shared.ProgressTrackerClient.Mapping
                 .ForMember(dest => dest.Job, opt => opt.MapFrom(src => src.Job))
                 .ReverseMap();
 
-            // GetJobsResult
             CreateMap<GetJobsResult, Contracts.Models.GetJobsResult>()
                 .ForMember(dest => dest.Jobs, opt => opt.MapFrom(src => src.Jobs))
                 .ReverseMap();
 
-            // Job - улучшенный маппинг с обработкой null значений
             CreateMap<Job, Contracts.Models.Job>()
                 .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
                 .ForMember(dest => dest.FinishedAt, opt => opt.MapFrom(src => 
@@ -44,7 +41,9 @@ namespace SaxsSpot.Shared.ProgressTrackerClient.Mapping
                 .ForMember(dest => dest.FinishedAt, opt => opt.MapFrom(src => 
                     src.FinishedAt.HasValue ? Timestamp.FromDateTime(src.FinishedAt.Value) : null));
 
-            // Остальные маппинги
+            CreateMap<ChangeJobMessageQuery, Contracts.Models.ChangeJobMessageQuery>()
+                .ReverseMap();
+            
             CreateMap<CreateJobQuery, Contracts.Models.CreateJobQuery>()
                 .ReverseMap();
 
